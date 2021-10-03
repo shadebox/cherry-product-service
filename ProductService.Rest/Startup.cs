3e6fa5d70d9544fadc76237d94be84c71367ccd3
Sanaan <sanaan@live.ca>
2021-10-03 18:07:52 -0400

    Initializing rest api proejct

---
+++ ProductService.Rest/Startup.cs
@@ -0,0 +1,57 @@
+#region Include Definition
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+#endregion
+
+namespace ProductService.Rest
+{
+    #region Public Class Definition
+    public class Startup
+    {        
+        #region Public Property Definition
+        public IConfiguration Configuration { get; set; }
+        #endregion
+        
+        #region Public Constructor Definition
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+        #endregion
+
+        #region Public Method Definition
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+            services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+        #endregion
+    }
+    #endregion
+}
