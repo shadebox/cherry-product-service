9eddad75e81051d15a8fc6238621535a898153df
Sanaan <49257534+shadebox@users.noreply.github.com>
2021-10-03 21:40:28 -0400

    cha-18 Setting up rest api project (#4)
    * Initializing rest api proejct
    
    * Updating cors

---
+++ ProductService.Rest/Program.cs
@@ -0,0 +1,34 @@
+#region Include Definition
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+#endregion
+
+namespace ProductService.Rest
+{
+    #region Public Class Definition
+    public class Program
+    {
+        #region Public Method Definition
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) 
+        {
+            return Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+        }            
+        #endregion
+    }
+    #endregion
+}
