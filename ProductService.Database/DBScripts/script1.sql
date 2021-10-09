8ae5e21fb52e326ead467423a5a17d76f0ef0f62
Sanaan <sanaan@live.ca>
2021-10-09 19:48:49 -0400

    Adding auto mapper and profile class to complete Get endpoint

--- ProductService.Database/DBScripts/script1.sql
+++ ProductService.Database/DBScripts/script1.sql
@@ -6,14 +6,14 @@ USE ProductService;
 
 -- Create product table
  CREATE TABLE Products (
-    ProductID INT IDENTITY(1,1) PRIMARY KEY,
+    ProductID BIGINT IDENTITY(1,1) PRIMARY KEY,
     Name VARCHAR(32) NOT NULL,
     Description TEXT,
     Specification TEXT,
     Delivery TEXT,
     ModelNumber VARCHAR(16) NOT NULL,    
-    CreateDate datetime2,    
-    ModifiedDate datetime2,
+    CreateDate DATETIME2 NOT NULL DEFAULT GETDATE(),    
+    ModifiedDate DATETIME2 NOT NULL DEFAULT GETDATE(),
     Timestamp ROWVERSION,
     Status TINYINT NOT NULL
 );
--- ProductService.Rest/Controllers/ProductController.cs
+++ ProductService.Rest/Controllers/ProductController.cs
@@ -4,6 +4,9 @@ using ProductService.BusinessLogic.Service;
 using System.Threading.Tasks;
 using System.Collections.Generic;
 using System;
+using AutoMapper;
+using ProductService.Rest.Models.Resources;
+using ProductService.Database.Domain;
 #endregion
 
 namespace ProductService.Rest.Controllers
@@ -14,21 +17,24 @@ namespace ProductService.Rest.Controllers
     {
         #region Private Field Definition
         private readonly IProductService _productService;
+        private readonly IMapper _mapper;
         #endregion
 
         #region Public Constructor Definition
-        public ProductController(IProductService productService)
+        public ProductController(IProductService productService, IMapper mapper)
         {
             _productService = productService;
+            _mapper = mapper;
         }
         #endregion
 
         #region Public Method Definition
         [HttpGet]
-        public async Task<string> GetProductAsync()
+        public async Task<ProductResource> GetProductAsync()
         {
-            var p = await _productService.GetProductAsync(1);
-            return p?.Name;
+            var product = await _productService.GetProductAsync(1);
+            
+            return _mapper.Map<Product, ProductResource>(product);
         }
         #endregion
     }
